5f43180832a51716a1465e18c999a4e272635f52
Roman Grigoryev <Roman_Grigoryev@xyratex.com>
2012-02-10 21:39:26 +0400

    enable gathering lustre diagnostic add file
    Change-Id: Ie0eee34769732a09216b26ed1f9a72fa27d2d08e

---
+++ lib/XTest/Executor/Roles/GetDiagnostics.pm
@@ -0,0 +1,69 @@
+#
+#===============================================================================
+#
+#         FILE:  GetDiagnostics.pm
+#
+#  DESCRIPTION:  Call lustre-diagnostic on every node and get xml results
+#       AUTHOR:  ryg
+#      COMPANY:  Xyratex
+#      CREATED:  02/10/2012 03:55:40 PM
+#===============================================================================
+
+=pod
+
+=head1 NAME
+
+XTest::Executor::Roles::GetDiagnostics - gathering information on node after test failure
+
+=head1  DESCRIPTION
+
+The module contains function which called after every execution for harvest diagnostic information from nodes which defined in configuration.
+
+The module requests setup of lustre-diagnostic package on every node in cluster.
+
+Result of the module is file C<(testid).diagnostic.xml.(node id>.log>
+
+For enable this module for test or test group add to C<roles> property string C<GetDiagnostics>  
+
+=cut
+
+package XTest::Executor::Roles::GetDiagnostics;
+
+use strict;
+use warnings;
+
+use Moose::Role;
+use Time::HiRes;
+use XTest::Utils;
+
+requires    'env', 'addMessage', 'getNormalizedLogName', 'registerLogFile';
+
+after 'execute' => sub {
+    my $self = shift;
+  if ( ( $self->yaml->{status_code} ) == 1 ) {    #test failed
+   foreach my $n (@{$self->env->nodes}){
+        my $c = $n->getExclusiveRC;
+        my $td = '/tmp/xpdiagnostic.'
+                    .Time::HiRes::gettimeofday().'.xml';
+        
+        $c->createSync("/usr/sbin/lustre-diagnostics -x $td",60);
+
+        my $res = $c->getFile( $td,
+            $self->getNormalizedLogName('diagnostic.xml.'.$n->id));
+            if ($res == 0){
+                $self->registerLogFile('diagnostic.xml.'.$n->id,
+                     $self->getNormalizedLogName
+                                ('diagnostic.xml.'.$n->id));
+            }else{
+                $self->addMessage(
+                    "Cannot copy log file [$td]: $res");
+
+            }
+        
+
+        }
+   }
+
+};
+
+1;
