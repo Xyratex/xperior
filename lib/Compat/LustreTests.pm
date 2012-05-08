2857373428d72054ffe3cbfd5ecfc5d1f389396f
Roman Grigoryev <Roman_Grigoryev@xyratex.com>
2012-05-08 14:00:20 +0400

    Added functionaly for  generating test descriptors from raw lusre shell test files
    Change-Id: I741538eda80eb9fae88731bded28686bc97bccec

---
+++ lib/Compat/LustreTests.pm
@@ -0,0 +1,179 @@
+#
+#===============================================================================
+#
+#         FILE:  LustreTests.pm
+#
+#  DESCRIPTION:  Set of functions to work with old bash lustre test
+#
+#       AUTHOR:  ryg
+#      COMPANY:  Xyratex
+#      CREATED:  05/07/2012 07:34:30 PM
+#===============================================================================
+package Compat::LustreTests;
+use strict;
+use warnings;
+
+use Log::Log4perl qw(:easy);
+use File::Basename;
+use English;
+use Carp;
+use Cwd;
+use YAML qw "Bless LoadFile Load Dump";
+my $XPERIORBINDIR;
+our @ISA;
+our @EXPORT;
+
+BEGIN {
+    @ISA           = ("Exporter");
+    @EXPORT        = qw(&writeTagsFile &writeGeneratedTestSuiteFile &getGeneratedTestSuite);
+    $XPERIORBINDIR = dirname( Cwd::abs_path($PROGRAM_NAME) );
+    push @INC, "$XPERIORBINDIR/../lib";
+    Log::Log4perl->easy_init( { level => $DEBUG } );
+
+}
+use constant LUSTRETESTS => '/usr/lib64/lustre/tests';
+
+=item  writeTestSuiteFile($testdir, $testsuite, $predefineddir, $sourcedir)
+    $testdir       - where store new test descriptor
+    
+    $testsuite     - name of test suite (test group) whose descriptor will                    be generated
+    
+    $predefineddir - directory, where stored test descriptor, values from                     whose will be added to new test suite descriptor
+
+    $sourcedir     - where original lustre test scripts are
+
+=cut
+
+sub writeGeneratedTestSuiteFile {
+    my ($testdir, $tsname, $pdir, $sdir ) = @_;
+    my $yaml = getGeneratedTestSuite($tsname, $pdir, $sdir);
+    $YAML::Stringify = 1;
+    my $file = "$testdir/${tsname}_tests.yaml";
+    open REP, "> $file" or confess "Cannot open report file:" . $!;
+    print REP Dump($yaml);
+    close REP;
+}
+
+sub getGeneratedTestSuite{
+    my ($tsname, $pdir, $sdir ) = @_;
+    my $script = "$sdir/$tsname.sh";
+
+    unless ( -e "$script" ) {
+        confess "Cannot open test script [$script]";
+    }
+    my $ts = _generateTestSuite( $tsname, $script );
+    $ts = _mergeWithPreconfig( $ts, $tsname, $pdir );
+    return $ts;
+}
+
+sub _mergeWithPreconfig {
+    my ( $ts, $tsname, $pdir ) = @_;
+    my $pcfgscr = "$pdir/${tsname}_tests.yaml";
+    unless ( -e $pcfgscr ) {
+        WARN "Cannot open file [$pcfgscr], no merge done";
+        return $ts;
+    }
+    my $pcfg = LoadFile($pcfgscr);
+
+    #go over only
+    #use Data::Dumper;
+    foreach my $key ( keys %{$pcfg} ) {
+        if ( $key eq 'Tests' ) {
+            my $i = 0;
+            foreach my $t ( @{ $pcfg->{$key} } ) {
+                my $tid = $t->{'id'};
+                #print "test descr is" . Dumper $t;
+                DEBUG "Check test $tid description\n";
+
+                my $nto = undef;
+                foreach my $ntk ( @{ $ts->{$key} } ) {
+                    if ( $ntk->{'id'} eq $tid ) {
+                        $nto = $ntk;
+                        last;
+                    }
+                }
+                unless ( defined($nto) ) {
+                    INFO "\n Skip preconfigured object for test [$tid \n";
+                    next;
+                }
+
+                foreach my $tk ( keys %{$t} ) {
+                    unless ( defined( $nto->{$tk} ) ) {
+                        DEBUG "Found not set key {$tk} "
+                          . "for test $t->{'id'} \n";
+                        $nto->{$tk} = $t->{$tk};
+                    }
+                }
+                $i++;
+            }
+        }
+        else {
+            unless ( defined( $ts->{$key} ) ) {
+                $ts->{$key} = $pcfg->{$key};
+            }
+        }
+    }
+    return $ts;
+}
+
+#TODO create tests for it
+sub _generateTestSuite {
+    my ( $suitename, $script ) = @_;
+    INFO "Reading $script";
+    my $content = {
+        groupname   => $suitename,
+        description => "Lustre $suitename tests",
+        reference   => "http://wiki.lustre.org/index.php/Testing_Lustre_Code",
+        Tests       => [],
+    };
+
+    open( SCRIPT, "<$script" ) or confess "Cannot read file $script";
+    while (<SCRIPT>) {
+        if ( $_ =~
+/^(run_test|run_test_with_stat)\s+([0-9]+[A-Za-z]*)\s+\"([^\"]+)\"\s*$/
+          )
+        {
+            push @{ $content->{Tests} }, { id => $2 };
+            DEBUG "Found '$suitename' test $2: $3";
+        }
+    }
+    close SCRIPT;
+    INFO "Found tests: ", scalar @{ $content->{Tests} };
+    return $content;
+}
+
+#TODO write tests for it
+sub writeGeneratedTagsFile {
+    my ($testdir ) = @_;
+    my $yaml = _getTags();
+    $YAML::Stringify = 1;
+    my $file = "$testdir/tags.yaml";
+    open REP, "> $file" or confess "Cannot open report file:" . $!;
+    print REP Dump($yaml);
+    close REP;
+
+}
+
+sub _getTags {
+
+    #TODO read it from file
+    return {
+        tags => [
+            {
+                id   => 'performance.io',
+                name => 'I/O Performance',
+                description =>
+'typical clustered filesystem performance benchmarks. Test subtypes: shared-single-file, file-per-process'
+            },
+            {
+                id   => 'performance.md',
+                name => 'Metadata performance',
+                description =>
+'Test subtypes by operation and pattern (eg: directory-per-client create, shared directory create)'
+            },
+        ]
+    };
+}
+
+1;
+
---
+++ t/lustre/tests/replay-ost-single.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+
+set -e
+
+LUSTRE=${LUSTRE:-$(cd $(dirname $0)/..; echo $PWD)}
+SETUP=${SETUP:-""}
+CLEANUP=${CLEANUP:-""}
+. $LUSTRE/tests/test-framework.sh
+init_test_env $@
+. ${CONFIG:=$LUSTRE/tests/cfg/$NAME.sh}
+init_logging
+
+# While we do not use OSTCOUNT=1 setup anymore,
+# ost1failover_HOST is used
+#ostfailover_HOST=${ostfailover_HOST:-$ost_HOST}
+#failover= must be defined in OST_MKFS_OPTIONS if ostfailover_HOST != ost_HOST
+
+require_dsh_ost || exit 0
+
+# Tests that fail on uml
+CPU=`awk '/model/ {print $4}' /proc/cpuinfo`
+[ "$CPU" = "UML" ] && EXCEPT="$EXCEPT 6"
+
+# Skip these tests
+# BUG NUMBER: 
+ALWAYS_EXCEPT="$REPLAY_OST_SINGLE_EXCEPT"
+
+#					
+[ "$SLOW" = "no" ] && EXCEPT_SLOW="5"
+
+build_test_filter
+
+check_and_setup_lustre
+assert_DIR
+rm -rf $DIR/[df][0-9]*
+
+TDIR=$DIR/d0.${TESTSUITE}
+mkdir -p $TDIR
+$LFS setstripe $TDIR -i 0 -c 1
+$LFS getstripe $TDIR
+
+test_0a() {
+    zconf_umount `hostname` $MOUNT -f
+    # needs to run during initial client->OST connection
+    #define OBD_FAIL_OST_ALL_REPLY_NET       0x211
+    do_facet ost1 "lctl set_param fail_loc=0x80000211"
+    zconf_mount `hostname` $MOUNT && df $MOUNT || error "0a mount fail"
+}
+run_test 0a "target handle mismatch (bug 5317) `date +%H:%M:%S`"
+
+test_0b() {
+    fail ost1
+    cp /etc/profile  $TDIR/$tfile
+    sync
+    diff /etc/profile $TDIR/$tfile
+    rm -f $TDIR/$tfile
+}
+run_test 0b "empty replay"
+
+test_1() {
+    date > $TDIR/$tfile || error "error creating $TDIR/$tfile"
+    fail ost1
+    $CHECKSTAT -t file $TDIR/$tfile || return 1
+    rm -f $TDIR/$tfile
+}
+run_test 1 "touch"
+
+test_2() {
+    for i in `seq 10`; do
+        echo "tag-$i" > $TDIR/$tfile-$i || error "create $TDIR/$tfile-$i"
+    done 
+    fail ost1
+    for i in `seq 10`; do
+      grep -q "tag-$i" $TDIR/$tfile-$i || error "grep $TDIR/$tfile-$i"
+    done 
+    rm -f $TDIR/$tfile-*
+}
+run_test 2 "|x| 10 open(O_CREAT)s"
+
+test_3() {
+    verify=$ROOT/tmp/verify-$$
+    dd if=/dev/urandom bs=4096 count=1280 | tee $verify > $TDIR/$tfile &
+    ddpid=$!
+    sync &
+    fail ost1
+    wait $ddpid || return 1
+    cmp $verify $TDIR/$tfile || return 2
+    rm -f $verify $TDIR/$tfile
+}
+run_test 3 "Fail OST during write, with verification"
+
+test_4() {
+    verify=$ROOT/tmp/verify-$$
+    dd if=/dev/urandom bs=4096 count=1280 | tee $verify > $TDIR/$tfile
+    # invalidate cache, so that we're reading over the wire
+    cancel_lru_locks osc
+    cmp $verify $TDIR/$tfile &
+    cmppid=$!
+    fail ost1
+    wait $cmppid || return 1
+    rm -f $verify $TDIR/$tfile
+}
+run_test 4 "Fail OST during read, with verification"
+
+iozone_bg () {
+    local args=$@
+
+    local tmppipe=$TMP/${TESTSUITE}.${TESTNAME}.pipe
+    mkfifo $tmppipe
+
+    echo "+ iozone $args"
+    iozone $args > $tmppipe &
+
+    local pid=$!
+
+    echo "tmppipe=$tmppipe"
+    echo iozone pid=$pid
+
+    # iozone exit code is 0 even if iozone is not completed
+    # need to check iozone output  on "complete"
+    local iozonelog=$TMP/${TESTSUITE}.iozone.log
+    rm -f $iozonelog
+    cat $tmppipe | while read line ; do
+        echo "$line"
+        echo "$line" >>$iozonelog
+    done;
+
+    local rc=0
+    wait $pid
+    rc=$?
+    if ! $(tail -1 $iozonelog | grep -q complete); then
+        echo iozone failed!
+        rc=1
+    fi
+    rm -f $tmppipe
+    rm -f $iozonelog
+    return $rc
+}
+
+test_5() {
+    [ -z "`which iozone 2> /dev/null`" ] && skip_env "iozone missing" && return 0
+
+    # striping is -c 1, get min of available
+    local minavail=$(lctl get_param -n osc.*[oO][sS][cC][-_]*.kbytesavail | sort -n | head -1)
+    local size=$(( minavail * 3/4 ))
+    local GB=1048576  # 1048576KB == 1GB
+
+    if (( size > GB )); then
+        size=$GB
+    fi
+    local iozone_opts="-i 0 -i 1 -i 2 -+d -r 4 -s $size -f $TDIR/$tfile"
+
+    iozone_bg $iozone_opts &
+    local pid=$!
+
+    echo iozone bg pid=$pid
+
+    sleep 8
+    fail ost1
+    local rc=0
+    wait $pid
+    rc=$?
+    log "iozone rc=$rc"
+    rm -f $TDIR/$tfile
+    [ $rc -eq 0 ] || error "iozone failed"
+    return $rc
+}
+run_test 5 "Fail OST during iozone"
+
+kbytesfree() {
+   calc_osc_kbytes kbytesfree
+}
+
+test_6() {
+    remote_mds_nodsh && skip "remote MDS with nodsh" && return 0
+
+    f=$TDIR/$tfile
+    rm -f $f
+    sync && sleep 2 && sync	# wait for delete thread
+    before=`kbytesfree`
+    dd if=/dev/urandom bs=4096 count=1280 of=$f || return 28
+    lfs getstripe $f
+    get_stripe_info client $f
+
+    sync
+    sleep 2 # ensure we have a fresh statfs
+    sync
+#define OBD_FAIL_MDS_REINT_NET_REP       0x119
+    do_facet $SINGLEMDS "lctl set_param fail_loc=0x80000119"
+    after_dd=`kbytesfree`
+    log "before: $before after_dd: $after_dd"
+    (( $before > $after_dd )) || return 1
+    rm -f $f
+    fail ost$((stripe_index + 1))
+    wait_recovery_complete ost$((stripe_index + 1)) || error "OST recovery not done"
+    $CHECKSTAT -t file $f && return 2 || true
+    sync
+    # let the delete happen
+    wait_mds_ost_sync || return 4
+    wait_destroy_complete || return 5
+    after=`kbytesfree`
+    log "before: $before after: $after"
+    (( $before <= $after + 40 )) || return 3	# take OST logs into account
+}
+run_test 6 "Fail OST before obd_destroy"
+
+test_7() {
+    f=$TDIR/$tfile
+    rm -f $f
+    sync && sleep 5 && sync	# wait for delete thread
+    before=`kbytesfree`
+    dd if=/dev/urandom bs=4096 count=1280 of=$f || return 4
+    sync
+    sleep 2 # ensure we have a fresh statfs
+    sync
+    after_dd=`kbytesfree`
+    log "before: $before after_dd: $after_dd"
+    (( $before > $after_dd )) || return 1
+    replay_barrier ost1
+    rm -f $f
+    fail ost1
+    wait_recovery_complete ost1 || error "OST recovery not done"
+    $CHECKSTAT -t file $f && return 2 || true
+    sync
+    # let the delete happen
+    wait_mds_ost_sync || return 4
+    wait_destroy_complete || return 5
+    after=`kbytesfree`
+    log "before: $before after: $after"
+    (( $before <= $after + 40 )) || return 3	# take OST logs into account
+}
+run_test 7 "Fail OST before obd_destroy"
+
+complete $(basename $0) $SECONDS
+check_and_cleanup_lustre
+exit_status
