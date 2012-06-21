4c04685c80b4ac95ee4d94899f7e4594be29cd24
Roman Grigoryev <Roman_Grigoryev@xyratex.com>
2012-06-21 17:33:40 +0400

    simple fixes in GetCoverage role
    Change-Id: Iecf56b53ce78163a47daa4e39327ed4bac35d0cc

--- lib/Xperior/Executor/Roles/GetCoverage.pm
+++ lib/Xperior/Executor/Roles/GetCoverage.pm
@@ -32,7 +32,7 @@ before 'execute' => sub {
                 " mount -t debugfs none /sys/kernel/debug", 60 );
         
         DEBUG $c->createSync( 
-                " lcov --zerocounters", 60 );
+                " lcov --zerocounters ", 60 );
         last;
     }
 
@@ -46,24 +46,60 @@ after   'execute' => sub{
         my $c = $n->getExclusiveRC;
 
         DEBUG $c->createSync(
-                "lcov --no-checksum "
-                ."-b /root/cov/lustre-wc-rel/ "#fixed lustre paths
-                ."-d /root/cov/lustre-wc-rel/ "
-                #."-b /lib/modules/2.6.32/build/ " #fixed kernel
-                #."--remove fullcoverage.trace '*kernel*' "
-                ."--capture --output-file /tmp/coverage.$n->{id} ",
+                 " lcov --no-checksum --ignore-errors source "
+                ." -b /root/cov/lustre-wc-rel/ "#fixed lustre paths
+                ." --capture --output-file /tmp/coverage.$n->{id} 2&>1 > /dev/null ",
                 300 );
-            my $res = $c->getFile( "/tmp/coverage.$n->{id}",
-            $self->getNormalizedLogName('coverage.'.$n->id));
-            if ($res == 0){
-                $self->registerLogFile('coverage.'.$n->id,
-                     $self->getNormalizedLogName('coverage.'.$n->id));
-            }else{
-                $self->addMessage(
-                    "Cannot attach coverage file log [coverage.$n->{id}]: $res");
+       my  $syncres = $c->syncexitcode;
+        if ($syncres != 0){
+            DEBUG "Cannot collect kernel lcov data, exit code is [$syncres]";
+            exit 99;
+        }
 
-            }
-            
+        DEBUG `rm -rf "/tmp/coverage.$n->{id}"`;
+        my $res = $c->getFile( "/tmp/coverage.$n->{id}","/tmp/coverage.$n->{id}");
+
+        my $cmd = 
+                "perl ".$ENV{'WORKSPACE'}."/scripts/coverage/lcov_filter.pl "
+                ." -s /tmp/coverage.$n->{id} "
+                ." -o ".$self->getNormalizedLogName('coverage.'.$n->id)
+                ." -p 'lnet'  -p 'libcfs' -p 'lustre-wc-rel.lustre'  ";
+        DEBUG `$cmd`;
+
+        
+        if ($res == 0){
+            $self->registerLogFile('coverage.'.$n->id,
+                $self->getNormalizedLogName('coverage.'.$n->id));
+        }else{
+            $self->addMessage(
+                "Cannot attach coverage file log [coverage.$n->{id}]: $res");
+        }
+
+        DEBUG $c->createSync(
+                 "lcov --no-checksum "
+                #."-b /root/cov/lustre-wc-rel/ "#fixed lustre paths
+                ." -d /root/cov/lustre-wc-rel/ "
+                ." -k /lib/modules/2.6.32/build/ " #fixed kernel
+                #."--remove fullcoverage.trace '*kernel*' "
+                ." --capture --output-file /tmp/usercoverage.$n->{id} 2&>1 > /dev/null ",
+                300 );
+        $syncres = $c->syncexitcode;
+        if ($syncres != 0){
+            DEBUG "Cannot collect user lcov data, exit code is [$syncres]";
+            exit 99;
+        }
+
+        $res = $c->getFile( "/tmp/usercoverage.$n->{id}",
+            $self->getNormalizedLogName('usercoverage.'.$n->id));
+        if ($res == 0){
+            $self->registerLogFile('usercoverage.'.$n->id,
+                $self->getNormalizedLogName('usercoverage.'.$n->id));
+        }else{
+            $self->addMessage(
+                "Cannot attach coverage file log [usercoverage.$n->{id}]: $res");
+
+        }
+
         last;
     }
 };
