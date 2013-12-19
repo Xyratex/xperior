ff2477bc3c14f40df677dc0a4cbed89b0a54f521
Roman Grigoryev <Roman_Grigoryev@xyratex.com>
2013-12-19 14:56:42 +0400

    Fix for RTP-1228 : add sysrq-t output to test logs
    Change-Id: I89bb08022447d7f802c025b25082902d83faddf5

---
+++ lib/Xperior/Executor/Roles/StacktraceGenerator.pm
@@ -0,0 +1,104 @@
+#
+# GPL HEADER START
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 only,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License version 2 for more details (a copy is included
+# in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU General Public License
+# version 2 along with this program; If not, see http://www.gnu.org/licenses
+#
+# Please  visit http://www.xyratex.com/contact if you need additional
+# information or have any questions.
+#
+# GPL HEADER END
+#
+# Copyright 2013 Xyratex Technology Limited
+#
+# Author: Roman Grigoryev<Roman_Grigoryev@xyratex.com>
+#
+=pod
+
+=head1 NAME
+
+Xperior::Executor::Roles::StacktraceGenerator - Role define generatig
+stacktrace into system console via /proc/sysrq-trigger usage.
+
+=head1 DESCRIPTION
+
+Info about sysrt : https://www.kernel.org/doc/Documentation/sysrq.txt
+
+The role could be used in cases when test failure detected and developer needs
+more infomation about current system state. When role switched on it going
+over all nodes in system configuration and call 'echo t > /proc/sysrq-trigger'
+and 'echo m > /proc/sysrq-trigger'.  
+Stacktrace will be stored in system console so, it is recommended to use
+StacktraceGenerator with NetconsoleCollector or StoreConsole.
+
+
+=cut
+
+package Xperior::Executor::Roles::StacktraceGenerator;
+use strict;
+use warnings;
+use Moose::Role;
+use Data::Dumper;
+use Log::Log4perl qw(:easy);
+
+has sysrqtcmd   => ( is =>'rw', default => 'echo t > /proc/sysrq-trigger');
+has sysrqmcmd   => ( is =>'rw', default => 'echo m > /proc/sysrq-trigger');
+has lctldkcmd   => ( is =>'rw', default => 'lctl dk');
+has sysrqt_timeout => ( is =>'rw', default => 120);
+has lctldk_timeout => ( is =>'rw', default => 120);
+
+after 'execute' => sub {
+	my $self      = shift;
+    foreach my $n (@{$self->env->nodes}) {
+    	if ( ( $self->yaml->{status_code} ) == 1 ) {    #test failed    		
+			my $c = $n->getExclusiveRC();
+			DEBUG("Call 'lctl dk' on node [".$n->ip()."]");
+			my $rlogfile="/tmp/lctl_dk.out.".time();
+			my $log = $c->createSync(
+				$self->lctldkcmd. " > $rlogfile",120);
+			$self->_getLog($c,$rlogfile,'lctl_dk.'.$n->ip());
+	        INFO("Call 'sysrq' commands on node [".$n->ip()."]");
+	        $c->createSync($self->sysrqtcmd(),3*60);
+	        $c->createSync($self->sysrqmcmd(),3*60);
+    	};
+    };
+};
+
+1;
+
+=head1 COPYRIGHT AND LICENSE
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 only,
+as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License version 2 for more details (a copy is included
+in the LICENSE file that accompanied this code).
+
+You should have received a copy of the GNU General Public License
+version 2 along with this program; If not, see http://www.gnu.org/licenses
+
+
+
+Copyright 2013 Xyratex Technology Limited
+
+=head1 AUTHOR
+
+Roman Grigoryev<Roman_Grigoryev@xyratex.com>
+
+=cut
